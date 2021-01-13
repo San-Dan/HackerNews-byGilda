4404fa5bfe4305f00718cbea7a2a24fbab206bc0
Gilda A <gilda.ahmadniaye@gmail.com>
2021-01-13 12:33:36 +0100

    just going through old stuff i did as help

--- index.php
+++ index.php
@@ -5,7 +5,19 @@ require __DIR__ . '/header.php';
 
 ?>
 
-<p> Here's all articles</p>
+
+
+<article>
+    <p> Here's all articles</p>
+        <div class="content-post">
+            <h2><?php echo $title; ?></h2>
+            <h3 class="author">Written by: <?php echo $name; ?></h3>
+            <h3 class="date">Published: <?php echo $date; ?></h3>
+            <img class="articleimage" src="<?php echo $image; ?>" alt="image">
+            <p class="text"><?php echo $text; ?> <a href="#">READ MORE</a> </p>
+            <button class="likeBtn <?php echo $likes; ?>"><i class="fa fa-thumbs-up" aria-hidden="true"></i>  <span><?php echo $likes; ?></span> likes</button>
+        </div>
+    </article>
 
 <?php endforeach; ?>
 
