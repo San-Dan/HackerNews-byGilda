01213a1ccf44a0dcd0d37cc910d906bb83aff4dc
Gilda A <gilda.ahmadniaye@gmail.com>
2021-01-27 12:18:15 +0100

    wow

---
+++ upvoted.php
@@ -0,0 +1,16 @@
+<?php require __DIR__ . '/app/autoload.php'; ?>
+<?php require __DIR__ . '/views/header.php'; ?>
+
+
+<button class="new-btn"><a href="/">New</a></button>
+        <button class="upvoted-btn active">Most upvoted</button>
+
+
+
+        
+<article>
+    <h1 class="glow"><?php echo $config['title']; ?></h1>
+</article>
+
+
+<?php require __DIR__ . '/views/footer.php'; ?>
